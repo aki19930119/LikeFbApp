17c2d041ebed576a8fc8faea6814c828c26da51a
SettlementApp <aki01191017@gmail.com>
2020-07-23 04:22:15 +0900

    Initial Commit

---
+++ LikeFbApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LikeFbApp
+//
+//  Created by 柿沼儀揚 on 2020/07/23.
+//  Copyright © 2020 柿沼儀揚. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
